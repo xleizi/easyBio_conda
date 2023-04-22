cb4477371bf708ccfea69829587a9b2cd3817357
xleizi <cuilei798@qq.com>
2023-04-22 23:17:12 +0800

    第一次正式

--- easyBio/run_cellranger.py
+++ easyBio/run_cellranger.py
@@ -1,31 +1,6 @@
-import os
-import subprocess
 import sys
 
-
-def main(db, fq_dir, expectcellnum):
-    # 获取文件夹中的所有文件名
-    filenames = os.listdir(fq_dir)
-
-    # 提取SRRxxxxxx样本
-    samples = set()
-    for filename in filenames:
-        if filename.startswith("SRR"):
-            sample_id = filename.split("_")[0]
-            samples.add(sample_id)
-
-    # 遍历样本并运行cellranger count命令
-    for sample in samples:
-        file_path = os.path.join(fq_dir, sample)
-        cmd = f"""cellranger count --id={sample} \
---transcriptome={db} \
---fastqs={fq_dir} \
---sample={sample} \
---expect-cells={expectcellnum} \
---nosecondary"""
-        print(f"Running command for sample {sample}:")
-        print(cmd)
-        subprocess.run(cmd, shell=True)
+from easyBio.Utils.easyBioUtils import cellrangerRun
 
 
 if __name__ == "__main__":
@@ -37,4 +12,4 @@ if __name__ == "__main__":
     fq_dir = sys.argv[2]
     expectcellnum = sys.argv[3]
 
-    main(db, fq_dir, expectcellnum)
+    cellrangerRun(db, fq_dir, expectcellnum)
